053990415c14cd3ecd3aed41b6eb81323d94a8c5
pcquynh <pcquynh@mun.ca>
2021-12-02 11:57:39 -0330

    Add outline for Blackjack game

---
+++ Blackjack.py
@@ -0,0 +1,37 @@
+def display_menu():
+
+
+def player_info():
+
+
+def give_cards():
+
+
+def show_card():
+
+
+def hit_card():
+
+
+def stand_card():
+
+def count_score_without_Ace():
+
+
+def count_score():
+
+
+def is_blackjack():
+
+
+def play_game():
+
+    
+def print_result():
+
+
+def main():
+
+
+if __name__ == '__main__':
+    main()
